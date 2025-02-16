6b2bbefd7c9e37837c56fee837e896ece6305688
Le Ngo Duc Anh <anhducngole2001@gmail.com>
2025-02-16 19:30:44 +0700

    feat: add packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,10 +9,12 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			routes: { exclude: ['<all>', '/sitemap.xml', '/robots.txt'] }
+		}),
+		alias: {
+			'$i18n/*': './src/lib/libs/i18n/messages/*'
+		}
 	},
 
 	extensions: ['.svelte', '.svx']
